d32fbfca1bdcdedffd5c22b5e682a9c6825c5826
Alexandre-Lucas-Dias <alexandrelucasdiascosta@yahoo.com.br>
2025-03-25 11:37:57 -0300

    cdc

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+const num1 = 5;
+const num2 = 7;
+
+function soma(num1, num2){
+    return num1 + num2;
+}
+
+function subtracao(num1, num2){
+    return num1 - num2;
+}
+
+function multiplicacao(num1, num2){
+    return num1 * num2;
+}
+
+function divisao(num1, num2){
+    return num1 / num2;
+}
+
+console.log(`A soma entre ${num1} e ${num2} é igual a ${soma(num1, num2)}`);
+console.log(`A subtração entre ${num1} e ${num2} é igual a ${subtracao(num1, num2)}`);
+console.log(`O produto entre ${num1} e ${num2} é igual a ${multiplicacao(num1, num2)}`);
+console.log(`A razão entre ${num1} e ${num2} é igual a ${divisao(num1, num2)}`);
